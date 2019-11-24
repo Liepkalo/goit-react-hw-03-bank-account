ad69c23de2cdad60b6357254642ab37f7db47159
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-11-24 20:53:54 +0200

    Updates

---
+++ precache-manifest.1e31e7dadddede32e446d92ce4183f06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294d3c6d96d222a9d440aecd976cac36",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "6fe9cc16328ba1f56e48",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.7c60da1b.chunk.css"
+  },
+  {
+    "revision": "e4261a596537d278a515",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.c287296b.chunk.js"
+  },
+  {
+    "revision": "6fe9cc16328ba1f56e48",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.e838ed06.chunk.js"
+  },
+  {
+    "revision": "6dcc9dbe649891159920",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.4efc9152.js"
+  }
+]);
